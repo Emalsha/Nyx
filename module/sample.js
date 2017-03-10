bb3dbc7df96bf6c619358d9d4a48dd60c8570c6f
Emalsha <remalsha@gmail.com>
2017-03-10 10:25:02 +0530

    used aira2js to connect with aria2c using rpc

--- module/sample.js
+++ module/sample.js
@@ -1,10 +1,43 @@
 /**
  * Created by emalsha on 3/9/17.
  */
-var aria = require('aria2');
+var Aria2 = require('aria2');
 
-if(aria){
-    console.log(aria);
+var aria2 = new Aria2();
+
+if(aria2){
+    console.log(Aria2);
 }
 
-aria.addUri('https://www.w3school.com/css/trolltunga.jpg');
+aria2.onopen = function(){
+    console.log('Aria2 open');
+};
+
+aria2.onclose = function(){
+    console.log('Aria2 close');
+};
+
+aria2.onsend = function(msg){
+    // console.log('Aria2 got : '+msg);
+    console.log(msg);
+};
+
+// aria2.onmessage = function (msg) {
+//     let {method,params} = msg;
+//     console.log('Aria2 send : '+method);
+//     console.log('Aria2 '+params[0].gid);
+// };
+
+aria2.onDownloadStart = function (msg) {
+    console.log('Download start > GID : '+msg.gid);
+};
+
+aria2.onDownloadComplete = function (msg) {
+    console.log('Download completed on > GID :'+msg.gid);
+};
+
+aria2.open().then(function () {
+    console.log('Web socket is open');
+}).then(aria2.addUri(['https://www.w3schools.com/css/trolltunga.jpg'],function (err, res) {
+    //console.log(err || res);
+}));
