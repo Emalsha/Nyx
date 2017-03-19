a3dc3e210ea02d97fa45fb16acc98effa42d3360
Emalsha <remalsha@gmail.com>
2017-03-20 01:16:32 +0530

    Network usage diagram done - change test

--- routes/info.js
+++ routes/info.js
@@ -9,8 +9,8 @@ var fixed_array = require('fixed-array');
 
 var rxArray = fixed_array(12);
 var txArray = fixed_array(12);
-router.get('/network_usage', function(req, res, next) {
 
+setInterval(function () {
     var rx = netstat.raw().wlp2s0.bytes.receive;
     rx = (rx/(1000*1000*1000));
     rxArray.push(rx);
@@ -18,6 +18,10 @@ router.get('/network_usage', function(req, res, next) {
     var tx = netstat.raw().wlp2s0.bytes.transmit;
     tx = (tx/(1000*1000*1000));
     txArray.push(tx);
+
+},1000*60*5);
+
+router.get('/network_usage', function(req, res, next) {
 
     var sendArray = [rxArray,txArray];
     res.send(sendArray);
