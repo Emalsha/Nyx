e43d44f9e36132b3971dc46d2c3570d35af310e1
Emalsha <remalsha@gmail.com>
2017-04-15 16:05:12 +0530

    role base permission granting done

--- module/acl_fn.js
+++ module/acl_fn.js
@@ -9,35 +9,25 @@ acl = new acl(new acl.memoryBackend());
 
 // create roles and give permission
 acl.allow('admin','administration','view');
-acl.allow('user1','user','*');
+acl.allow('user1',['/users/user','/users/myfile','/users/publicfile','/users/help'],['get','post']);
 
 // add users to roles
 acl.addUserRoles('Emalsha', 'user1');
 acl.addUserRoles('Sulochana','admin');
 
-module.exports = acl;
+module.exports.aclobj = acl;
 
-// module.exports = function(){
-//     return function(req,resp,next){
-//         let u = req.session.passport.user;
-//         debug('User '+ req.session.passport.user + ' verifying.');
-//         acl.isAllowed(u,'administrator', 'view',function (err, res) {
-//             resp.locals.isAdmin = res;
-//
-//             debug(req.url + ' ' + res);
-//             if(req.url == '/administration' && !res){
-//                 req.flash('error',"You not have permission!");
-//                 // res.redirect('/users/user');
-//                 next(()=>{
-//                     res.render('user',{title:'NYX | USER',message:req.flash('success') || req.flash('error') ,user:{uname:req.user.username,name:req.user.fname +' '+ req.user.lname}});
-//                 })
-//             }
-//
-//             next();
-//         });
-//
-//     }
-// };
+module.exports.aclfnc = function(){
+    return function(req,resp,next){
+        let u = req.session.passport.user;
+        debug('User '+ req.session.passport.user + ' verifying.');
+        acl.isAllowed(u,'administrator', 'view',function (err, res) {
+            resp.locals.isAdmin = res;
+            next();
+        });
+
+    }
+};
 
 
 
