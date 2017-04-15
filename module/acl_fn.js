f6262cc5e0379039ba96a60ed80b0bc2adb571dc
Emalsha <remalsha@gmail.com>
2017-04-15 13:47:16 +0530

    role base previlages granting

--- module/acl_fn.js
+++ module/acl_fn.js
@@ -8,23 +8,36 @@ let debug =require('debug')('nyx:acl');
 acl = new acl(new acl.memoryBackend());
 
 // create roles and give permission
-acl.allow('admin','administrator','view');
+acl.allow('admin','administration','view');
+acl.allow('user1','user','*');
 
 // add users to roles
-acl.addUserRoles('Emalsha', 'guest');
+acl.addUserRoles('Emalsha', 'user1');
 acl.addUserRoles('Sulochana','admin');
 
-module.exports = function(){
-    return function(req,resp,next){
-        let u = req.session.passport.user;
-        debug('User '+ req.session.passport.user + ' verified.');
-        let is = acl.isAllowed(u,'administrator', 'view',function (err, res) {
-            resp.locals.isAdmin = res;
-            next();
-        });
+module.exports = acl;
 
-    }
-};
+// module.exports = function(){
+//     return function(req,resp,next){
+//         let u = req.session.passport.user;
+//         debug('User '+ req.session.passport.user + ' verifying.');
+//         acl.isAllowed(u,'administrator', 'view',function (err, res) {
+//             resp.locals.isAdmin = res;
+//
+//             debug(req.url + ' ' + res);
+//             if(req.url == '/administration' && !res){
+//                 req.flash('error',"You not have permission!");
+//                 // res.redirect('/users/user');
+//                 next(()=>{
+//                     res.render('user',{title:'NYX | USER',message:req.flash('success') || req.flash('error') ,user:{uname:req.user.username,name:req.user.fname +' '+ req.user.lname}});
+//                 })
+//             }
+//
+//             next();
+//         });
+//
+//     }
+// };
 
 
 
--- routes/users.js
+++ routes/users.js
@@ -6,28 +6,28 @@ const session = require('express-session');
 const acl = require('../module/acl_fn');
 
 /* GET users listing. */
-router.get('/user',acl(),function(req, res, next) {
+router.get('/user',acl.middleware(),function(req, res) {
     res.render('user',{title:'NYX | USER',message:req.flash('success'),user:{uname:req.user.username,name:req.user.fname +' '+ req.user.lname}});
 });
 
-router.get('/myfile',acl(),function(req,res,next){
-    res.render('myfile',{title:'NYX | My File',user:{uname:req.user.username,name:req.user.fname +' '+ req.user.lname}});
-});
-
-router.get('/publicfile',acl(),function(req,res,next){
-    res.render('publicfile',{title:'NYX | Public File',user:{uname:req.user.username,name:req.user.fname +' '+ req.user.lname}});
-});
-
-router.get('/management',acl(),function(req,res,next){
-    res.render('userManagement',{title:'NYX | User Management',user:{uname:req.user.username,name:req.user.fname +' '+ req.user.lname}});
-});
-
-router.get('/administration',acl(),function(req,res,next){
-  res.render('administrator',{title:'NYX | Administrator',user:{uname:req.user.username,name:req.user.fname +' '+ req.user.lname}});
-});
-
-router.get('/help',acl(),function(req,res,next){
-    res.render('help',{title:'NYX | Help',user:{uname:req.user.username,name:req.user.fname +' '+ req.user.lname}});
-});
+// router.get('/myfile',acl(),function(req,res){
+//     res.render('myfile',{title:'NYX | My File',user:{uname:req.user.username,name:req.user.fname +' '+ req.user.lname}});
+// });
+//
+// router.get('/publicfile',acl(),function(req,res){
+//     res.render('publicfile',{title:'NYX | Public File',user:{uname:req.user.username,name:req.user.fname +' '+ req.user.lname}});
+// });
+//
+// router.get('/management',acl(),function(req,res){
+//     res.render('userManagement',{title:'NYX | User Management',user:{uname:req.user.username,name:req.user.fname +' '+ req.user.lname}});
+// });
+//
+// router.get('/administration',acl(),function(req,res){
+//   res.render('administrator',{title:'NYX | Administrator',user:{uname:req.user.username,name:req.user.fname +' '+ req.user.lname}});
+// });
+//
+// router.get('/help',acl(),function(req,res){
+//     res.render('help',{title:'NYX | Help',user:{uname:req.user.username,name:req.user.fname +' '+ req.user.lname}});
+// });
 
 module.exports = router;
