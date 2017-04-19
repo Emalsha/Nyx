f23634ac878526e6649a867c7e3d35e1eb70c799
Emalsha <remalsha@gmail.com>
2017-04-19 23:51:46 +0530

    gc

--- model/memberManagement.js
+++ model/memberManagement.js
@@ -1,48 +1,48 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-const config = require('../DBconfig/database'); // conneting to the database
-
-//User schema
-const UserSchema = mongoose.Schema({
-	email :{
-		type : String, 
-		required : true
-	},
-	username :{
-		type :String,
-		required : true
-	},
-	password  : {
-		type :String,
-		required : true
-	}
-
-});
-
-const User = module.exports = mongoose.model('User', UserSchema);
-
-module.exports.getUserById = function(id, callback){
-	User.findById(id , callback);
-}
-
-module.exports.getUserByUsername = function(username, callback){
-	const query = {username : username}
-	User.findOne(query,callback);
-}
-
-module.exports.addUser = function(newUser, callback){
-	bcrypt.genSalt(10, (err, salt) => {
-		bcrypt.hash(newUser.password, salt , (err, hash) => {
-			//if(err) throw er;
-			newUser.password = hash;
-			newUser.save(callback);
-		});
-	});
-}
-
-module.exports.comparePassword = function(password, hash , callback){
-	bcrypt.compare(password, hash , (err , isMatch) => {
-		//if(err) throw err;
-		callback(null,isMatch);
-	});
-}
+// const mongoose = require('mongoose');
+// const bcrypt = require('bcryptjs');
+// const config = require('../DBconfig/database'); // conneting to the database
+//
+// //User schema
+// const UserSchema = mongoose.Schema({
+// 	email :{
+// 		type : String,
+// 		required : true
+// 	},
+// 	username :{
+// 		type :String,
+// 		required : true
+// 	},
+// 	password  : {
+// 		type :String,
+// 		required : true
+// 	}
+//
+// });
+//
+// const User = module.exports = mongoose.model('User', UserSchema);
+//
+// module.exports.getUserById = function(id, callback){
+// 	User.findById(id , callback);
+// }
+//
+// module.exports.getUserByUsername = function(username, callback){
+// 	const query = {username : username}
+// 	User.findOne(query,callback);
+// }
+//
+// module.exports.addUser = function(newUser, callback){
+// 	bcrypt.genSalt(10, (err, salt) => {
+// 		bcrypt.hash(newUser.password, salt , (err, hash) => {
+// 			//if(err) throw er;
+// 			newUser.password = hash;
+// 			newUser.save(callback);
+// 		});
+// 	});
+// }
+//
+// module.exports.comparePassword = function(password, hash , callback){
+// 	bcrypt.compare(password, hash , (err , isMatch) => {
+// 		//if(err) throw err;
+// 		callback(null,isMatch);
+// 	});
+// }
--- routes/admin.js
+++ routes/admin.js
@@ -6,12 +6,8 @@ const express = require('express');
 const router = express.Router();
 const acl = require('../module/acl_fn');
 const debug = require('debug')('nyx:adminRouter');
-const passport = require('passport');
-const jwt = require('jsonwebtoken');
-const config = require('../DBconfig/database');
-const User = require('../model/memberManagement');
-
 const Download = require('../model/Download');
+
 // Permission granting
 let aclf = acl.aclfnc();
 let aclfn = acl.aclobj.middleware(2,(req,res)=>{return req.user.username});
@@ -19,24 +15,7 @@ let aclfn = acl.aclobj.middleware(2,(req,res)=>{return req.user.username});
 router.get('/management',aclfn,aclf,function(req,res){
     res.render('userManagement',{title:'NYX | User Management',user:{uname:req.user.username,name:req.user.fname +' '+ req.user.lname}});
 });
-/////////// User Registration
-router.post('/register', (req, res , next) => {
-	let newUser = new User({
 
-		email : req.body.email,
-		username : req.body.username,
-		password : req.body.password
-	});
-
-	User.addUser(newUser, (err, user) => {
-		if(err){
-			res.json({success : false , msg :'Failed to connect Database'});
-		} else{
-			res.json({success : true , msg :'User is registered'});
-		}
-	});
-});
-////////////////////////////
 router.get('/administration',aclfn,aclf,function(req,res){
     Download.find({state:'pending'},(err,downloads)=>{
         if(err){
