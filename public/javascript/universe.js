26124e5b195833cf947db80b489f987020b2b3e3
Sulochana Kodituwakku <sulochana.456@live.com>
2017-05-09 22:01:44 +0530

    UI changes

---
+++ public/javascript/universe.js
@@ -0,0 +1,9 @@
+/**
+ * Created by sulochana on 5/9/17.
+ */
+$(document).ready(function(){
+    //run call on ready functions.
+    for (i = 0; i < call_on_load.length; i++) {
+        window[call_on_load[i]]();
+    }
+});
