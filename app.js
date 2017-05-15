407030ef87d6dfce19fe2f113dd432f57962bc43
namalkanishka <kanishkanamal5@gmail.com>
2017-05-15 08:32:38 +0530

    file download

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const admin = require('./routes/admin');
 const info = require('./routes/info');
 const download = require('./routes/download');
 const url = require('./routes/url');
+const view_f = require('./routes/view_f');
 
 const app = express();
 
@@ -67,7 +68,7 @@ app.use('/admin',admin);
 app.use('/info',info);
 app.use('/download',download);
 app.use('/url',url);
-
+app.use('/view_f', view_f);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   let err = new Error('Not Found');
--- public/javascript/universe.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by sulochana on 5/9/17.
- */
-$(document).ready(function(){
-    //run call on ready functions.
-    for (i = 0; i < call_on_load.length; i++) {
-        window[call_on_load[i]]();
-    }
-});
