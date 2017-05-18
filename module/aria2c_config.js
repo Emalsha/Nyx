320d0bdbaa5b3e2cc26a91e88f000198b0aa38dd
Emalsha <remalsha@gmail.com>
2017-05-19 03:10:06 +0530

    Download updating functions..

--- module/aria2c_config.js
+++ module/aria2c_config.js
@@ -3,6 +3,7 @@
  */
 const Aria2 = require('aria2');
 const debug = require('debug')('nyx:aria-config');
+const Download = require('../model/Download');
 
 let option = {
     secure: false,
@@ -15,11 +16,22 @@ let option = {
 const aria2 = new Aria2(option);
 
 aria2.onDownloadStart = function (msg) {
-    debug('Download start > GID : ' + msg.gid);
+
+    Download.findOneAndUpdate({gid: msg.gid}, {state: 'downloading'},(err,res)=>{
+        if(err){
+            debug('Downloading file update faile : '+err);
+        }
+    });
+    // debug('Download start > GID : ' + msg.gid);
 };
 
 aria2.onDownloadComplete = function (msg) {
-    debug('Download completed on > GID :' + msg.gid);
+    Download.findOneAndUpdate({gid: msg.gid}, {state: 'downloaded',download_end_date:new Date()},(err,res)=>{
+        if(err){
+            debug('Downloaded file update faile : '+err);
+        }
+    });
+    // debug('Download completed on > GID :' + msg.gid);
 };
 
 aria2.onDownloadError = function (msg) {
