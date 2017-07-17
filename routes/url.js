31a5ae3ddcfffbe38e7377c140db3067e33d48f7
Sulochana Kodituwakku <sulochana.456@live.com>
2017-07-17 10:49:54 +0530

    Changed the kilo size from 1000 to 1024

--- routes/url.js
+++ routes/url.js
@@ -25,7 +25,7 @@ router.post('/validate', function (req, res) {
                     // Copy from Stackoverflow
                     function formatBytes(bytes, decimals) {
                         if (bytes === 0) return '0 Bytes';
-                        let k = 1000,
+                        let k = 1024,
                             dm = decimals || 2,
                             sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
                             i = Math.floor(Math.log(bytes) / Math.log(k));
